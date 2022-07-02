52aa38209a119e9cb006817493521deafae6d25d
victor <dvicxy@gmail.com>
2022-07-02 05:57:01 +0100

    initialising the goldrush class

--- lib/main.dart
+++ lib/main.dart
@@ -11,35 +11,9 @@ void main() async {
   await Flame.device.fullScreen();
   await Flame.device.setPortrait();
 
-  runApp(const MyApp());
+  runApp(GameWidget(game: goldrush));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
-  }
+class GoldRush with Loadable, Game {
+  static const int squareSpeed = 250;
 }
